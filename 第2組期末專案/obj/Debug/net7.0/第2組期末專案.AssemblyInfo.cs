81c43280cb1f6ba9056842d5b3f03a2e907d6e89
31 <wanks894@gmail.com>
2024-05-17 12:07:42 +0800

    aa

--- 第2組期末專案/obj/Debug/net7.0/第2組期末專案.AssemblyInfo.cs
+++ 第2組期末專案/obj/Debug/net7.0/第2組期末專案.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("第2組期末專案")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3198d18d1ec3c6ea875ed86637e29e7eec1d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3336480abc11488e5eae9658d337435854fc066")]
 [assembly: System.Reflection.AssemblyProductAttribute("第2組期末專案")]
 [assembly: System.Reflection.AssemblyTitleAttribute("第2組期末專案")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
