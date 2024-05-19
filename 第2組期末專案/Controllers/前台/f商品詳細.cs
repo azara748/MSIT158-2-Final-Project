1efd96cf3506f3d39cd5f8aaf0f24377d4388349
31 <wanks894@gmail.com>
2024-05-19 20:46:16 +0800

    0519

--- 第2組期末專案/Controllers/前台/f商品詳細.cs
+++ 第2組期末專案/Controllers/前台/f商品詳細.cs
@@ -8,19 +8,35 @@ namespace 第2組期末專案.Controllers.前台
         public IActionResult Index()
         {
             Random r = new Random();
-            int id= r.Next(1,200);
-			SelectShopContext db = new SelectShopContext();
+            int id = r.Next(1, 200);
+            SelectShopContext db = new SelectShopContext();
             TProduct a = null;
             while (a == null)
-            { 
+            {
                 a = db.TProducts.FirstOrDefault(x => x.ProductId == id);
                 id = r.Next(1, 200);
             }
             var b = db.TReviews.Join(db.TMembers, x => x.MemberId, y => y.MemberId, (x, y) =>
-            new { x.ReviewDate, x.RankId, x.Comment,y.MemberName});
-           ViewBag.b = b;
+            new { x.ReviewDate, x.RankId, x.Comment, y.MemberName });
+            ViewBag.b = b;
+            var c = db.TLabels.FirstOrDefault(x => x.LabelId == a.LabelId);
+            ViewBag.c = c;
             return View(a);
-		}      
+        }
+        [HttpPost]
+        public IActionResult Index(TCart c)
+        {
+            SelectShopContext db = new SelectShopContext();
+            var b = db.TCarts.FirstOrDefault(x => x.ProductId == c.ProductId&&x.MemberId==2);
+            if (b != null) b.Qty += c.Qty;
+            else
+            {
+            c.MemberId = 2;
+            db.TCarts.Add(c); 
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
 
---
+++ 第2組期末專案/Controllers/前台/f購物車.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using 第2組期末專案.Models;
+
+namespace 第2組期末專案.Controllers.前台
+{
+    public class f購物車 : Controller
+    {
+        public IActionResult Index()
+        {
+            int id = 2;
+            SelectShopContext db = new SelectShopContext();
+            var a = db.TCarts.Where(x => x.MemberId == id);
+            var b = a.Join(db.TProducts, x => x.ProductId, y => y.ProductId, (x, y) => new { y.ProductPhoto, y.ProductName, y.UnitPrice, y.Stocks, x.Qty, x.CartId });
+            ViewBag.b = b;
+            return View();
+            //https://localhost:7058/f%E8%B3%BC%E7%89%A9%E8%BB%8A/Index
+        }
+        public IActionResult Delete(int id)
+        {
+            SelectShopContext db = new SelectShopContext();
+            var a = db.TCarts.FirstOrDefault(x => x.CartId == id);
+            db.TCarts.Remove(a);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult 直接購買(int id, int Qty)
+        {
+            SelectShopContext db = new SelectShopContext();
+            var b = db.TCarts.FirstOrDefault(x => x.ProductId == id && x.MemberId == 2);
+            if (b != null) b.Qty += Qty;
+            {
+                TCart c = new TCart();
+                c.Qty = Qty;
+                c.ProductId = id;
+                c.MemberId = 2;
+                db.TCarts.Add(c);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
